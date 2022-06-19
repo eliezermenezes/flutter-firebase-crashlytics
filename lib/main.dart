5943c7f9f532217bcf04e4e69ca12a313055428d
Eliezer dos Santos Menezes <eliezermenezes19@gmail.com>
2022-06-19 17:55:09 -0400

    Flutter: Firebase Crashlytics

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:alura_crashlytics/screens/dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
+
+void main() async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+
+    if (kDebugMode) {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    } else {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      FirebaseCrashlytics.instance.setUserIdentifier('eliezermenezes@123');
+    }
+
+    runApp(BytebankApp());
+  }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Color.fromRGBO(71, 161, 56, 1),
+          textTheme: ButtonTextTheme.primary,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Color.fromRGBO(71, 161, 56, 1)),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
